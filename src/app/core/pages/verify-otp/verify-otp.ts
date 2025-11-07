62e65df43ddfc884b2869ee4c0600a5b42ffd34d
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-11-07 23:32:20 +0200

    apply verify-otp UI

--- src/app/core/pages/verify-otp/verify-otp.ts
+++ src/app/core/pages/verify-otp/verify-otp.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-verify-otp',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './verify-otp.html',
   styleUrl: './verify-otp.css',
 })
