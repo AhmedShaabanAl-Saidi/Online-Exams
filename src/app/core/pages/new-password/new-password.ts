f2e4109a1d3189e61d25216d4bb926ae978057df
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-11-07 22:32:17 +0200

    apply new-password UI

--- src/app/core/pages/new-password/new-password.ts
+++ src/app/core/pages/new-password/new-password.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-password',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './new-password.html',
   styleUrl: './new-password.css',
 })
 export class NewPassword {
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
 }
