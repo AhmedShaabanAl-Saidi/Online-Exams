aba7e6802b51add3e028e7d6935feb6d4b89cdd6
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-11-07 22:00:12 +0200

    apply register UI

--- src/app/core/pages/register/register.ts
+++ src/app/core/pages/register/register.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
 }
