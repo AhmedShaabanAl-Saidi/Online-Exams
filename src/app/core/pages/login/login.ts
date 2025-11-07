e1cc1130fd5f3f2c0807285dd1c92d7cb2b48000
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-11-07 22:11:23 +0200

    apply login UI

--- src/app/core/pages/login/login.ts
+++ src/app/core/pages/login/login.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
 }
--- src/app/core/pages/register/register.ts
+++ src/app/core/pages/register/register.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule, RouterLink],
+  imports: [RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
