0ed520bddad26200380ac213d897e3d1d3bc0d41
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-11-14 19:47:32 +0200

    update to Use feature based for for auth pages

--- src/app/core/feature/auth/pages/verify-otp/verify-otp.ts
+++ src/app/core/feature/auth/pages/verify-otp/verify-otp.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { MainBtn } from '../../../shared/components/UI/main-btn/main-btn';
+import { MainBtn } from '../../../../../shared/components/UI/main-btn/main-btn';
 
 @Component({
   selector: 'app-verify-otp',
