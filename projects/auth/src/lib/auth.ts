d7e2c67e8a2a2c0f3db40a3a4c1a0567c541ee8f
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-12-08 22:07:55 +0200

    apply auth service

--- projects/auth/src/lib/auth.ts
+++ projects/auth/src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { AuthAPI } from './base/auth-api';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AuthEndPoint } from './enums/auth-endpoint';
 import { AuthApiAdaptor } from './adaptor/auth-api-adaptor';
 
@@ -12,11 +12,72 @@ export class Auth implements AuthAPI {
   private _httpClient = inject(HttpClient);
   _authApiAdaptor = inject(AuthApiAdaptor);
 
+  private handleError(err: any) {
+    const errorMsg = err?.error?.message || 'Something went wrong';
+    const errorCode = err?.error?.code || 500;
+    return throwError(() => ({ message: errorMsg, code: errorCode }));
+  }
+
   login(credentials: any): Observable<any> {
     return this._httpClient.post(AuthEndPoint.LOGIN, credentials)
-      .pipe(map(res => this._authApiAdaptor.adapt(res)),
-        catchError(err => of(err))
+      .pipe(
+        catchError(err => this.handleError(err))
       );
   }
 
+  register(userData: any): Observable<any> {
+    return this._httpClient.post(AuthEndPoint.REGISTER, userData)
+      .pipe(
+        catchError(err => this.handleError(err))
+      );
+  }
+
+  logout(): Observable<any> {
+    return this._httpClient.get(AuthEndPoint.LOGOUT)
+      .pipe(
+        catchError(err => this.handleError(err))
+      );
+  }
+
+  forgotPassword(email: string): Observable<any> {
+    return this._httpClient.post(AuthEndPoint.FORGOTPASSWORD, { email })
+      .pipe(
+        catchError(err => this.handleError(err))
+      );
+  }
+
+  verifyResetCode(code: string): Observable<any> {
+    return this._httpClient.post(AuthEndPoint.VERIFYRESETCODE, { code })
+      .pipe(
+        catchError(err => this.handleError(err))
+      );
+  }
+
+  resetPassword(code: string, newPassword: string): Observable<any> {
+    return this._httpClient.put(AuthEndPoint.RESETPASSWORD, { code, newPassword })
+      .pipe(
+        catchError(err => this.handleError(err))
+      );
+  }
+
+  changePassword(current: string, newPassword: string): Observable<any> {
+    return this._httpClient.put(AuthEndPoint.CHANGEPASSWORD, { current, newPassword })
+      .pipe(
+        catchError(err => this.handleError(err))
+      );
+  }
+
+  deleteMyAccount(): Observable<any> {
+    return this._httpClient.get(AuthEndPoint.DELETEMYACCOUNT)
+      .pipe(
+        catchError(err => this.handleError(err))
+      );
+  }
+
+  editProfile(data: any): Observable<any> {
+    return this._httpClient.post(AuthEndPoint.EDITPROFILE, data)
+      .pipe(
+        catchError(err => this.handleError(err))
+      );
+  }
 }
